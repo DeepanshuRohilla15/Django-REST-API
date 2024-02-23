845be3b7e5a1b1b6d64f4a8b280163cfccde76e6
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-02-23 21:15:12 +0530

    Adding documentation

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,6 +3,7 @@ import requests
 #The requests module allows you to send HTTP requests using Python.
 #The HTTP request returns a Response Object with all the response data (content, encoding, status, etc).
 
+# Ways of request method
 # HTTP REQUEST -> HTML
 # REST API HTTP REQUEST -> JSON / XML
 # Javascript notation ~ Python Dictionary
